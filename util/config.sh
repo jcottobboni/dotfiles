764d01046f1cc5dede781ebca034bc40ccf7c1fb
João Carlos Ottobboni <jcottobboni@gmail.com>
2019-09-30 10:20:01 -0300

    Add Updates

--- util/config.sh
+++ util/config.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
-USER='jcottobboni'
+USER='joao.carlos'
 NAME='Joao Carlos Ottobboni'
 EMAIL='jcottobboni@gmail.com'
 
 PROJECTS="${HOME}/projects"
-DOCKERFILES='${HOME}/projects/dockerfiles'
-TERRAFORM='${HOME}/projects/tf_modules'
-PLAYBOOKS='${HOME}/projects/playbooks'
-CHEF_ROLES='${HOME}/projects/chef/roles'
-CHEF_ENVIRONMENTS='${HOME}/projects/chef/environments'
-CHEF_HANDLERS='${HOME}/projects/chef/handlers'
-CHEF_DATA_BAGS='${HOME}/projects/chef/chef-data-bags'
-CHEF_COOKBOOKS='${HOME}/projects/chef/chef-cookbooks'
-DCOS_CLUSTERS='${HOME}/projects/clusters/dcos-clusters'
+DOCKERFILES="${HOME}/projects/dockerfiles"
+TERRAFORM="${HOME}/projects/tf_modules"
+PLAYBOOKS="${HOME}/projects/playbooks"
+CHEF_ROLES="${HOME}/projects/chef/roles"
+CHEF_ENVIRONMENTS="${HOME}/projects/chef/environments"
+CHEF_HANDLERS="${HOME}/projects/chef/handlers"
+CHEF_DATA_BAGS="${HOME}/projects/chef/chef-data-bags"
+CHEF_COOKBOOKS="${HOME}/projects/chef/chef-cookbooks"
+DCOS_CLUSTERS="${HOME}/projects/clusters/dcos-clusters"
 BOOSTNOTE="${HOME}/Boostnote"
 PRIVATE_DOTFILES="${HOME}/dotfiles/private/dotfiles"
 SIEDOS_SCRIPTS="${HOME}/dotfiles/private/scripts"
